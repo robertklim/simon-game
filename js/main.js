5354a6fe62b33f21185865ad664470c8dacb7e71
Robert Klim <kontakt.robert.klim@gmail.com>
2018-11-05 12:06:14 +0100

    Generate random order, flash collors and play audio clips in that order for the set amount of turns

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 let order = [];
 let playerOrder = [];
+let flash;
 let turn;
+let good;
+let compTurn = false; // Computer flashing lights turn
+let intervalId;
+let audioOn = true;
 let strict = false;
 let on = false;
 let win;
@@ -31,3 +36,95 @@ onButton.addEventListener('change', (event) => {
         roundCounter.innerHTML = '';
     }
 });
+
+startButton.addEventListener('click', (event) => {
+    if (on || win) {
+        play();
+    }
+});
+
+function play() {
+    // Reset variables
+    win = false;
+    order = [];
+    playerOrder = [];
+    flash = 0;
+    intervalId = 0;
+    turn = 1;
+    roundCounter.innerHTML = 1;
+    good = true;
+    for (let i = 0; i < 20; i++) {
+        order.push(Math.floor(Math.random() * 4) + 1);
+    }
+
+    // console.log(order);
+
+    compTurn = true;
+
+    intervalId = setInterval(gameTurn, 1000);
+}
+
+function gameTurn() {
+    on = false; // To prevent player from clicking when computer is flashing
+    
+    if (flash === turn) {
+        clearInterval(intervalId);
+        compTurn = false;
+        clearColor();
+        on = true;
+    }
+
+    if (compTurn) {
+        clearColor();
+        setTimeout(() => {
+            if (order[flash] == 1) flashTopLeft();
+            if (order[flash] == 2) flashTopRight();
+            if (order[flash] == 3) flashBottomLeft();
+            if (order[flash] == 4) flashBottomRight();
+            flash++;
+        }, 200);
+    }
+}
+
+function flashTopLeft() {
+    if (audioOn) {
+        let audio = document.getElementById('clip1');
+        audio.play();
+    }
+    audioOn = true;
+    topLeft.style.backgroundColor = 'lightgreen';
+}
+
+function flashTopRight() {
+    if (audioOn) {
+        let audio = document.getElementById('clip2');
+        audio.play();
+    }
+    audioOn = true;
+    topRight.style.backgroundColor = 'red';
+}
+
+function flashBottomLeft() {
+    if (audioOn) {
+        let audio = document.getElementById('clip3');
+        audio.play();
+    }
+    audioOn = true;
+    bottomLeft.style.backgroundColor = 'yellow';
+}
+
+function flashBottomRight() {
+    if (audioOn) {
+        let audio = document.getElementById('clip4');
+        audio.play();
+    }
+    audioOn = true;
+    bottomRight.style.backgroundColor = 'lightskyblue';
+}
+
+function clearColor() {
+    topLeft.style.backgroundColor = 'darkgreen';
+    topRight.style.backgroundColor = 'darkred';
+    bottomLeft.style.backgroundColor = 'goldenrod';
+    bottomRight.style.backgroundColor = 'darkblue';
+}
